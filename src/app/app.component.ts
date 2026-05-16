46ff9e744cc32dc77ec7ddc660d136171e6b552b
RafiKastner <rafikastner@gmail.com>
2026-05-16 01:33:33 -0500

    use pnpm & working copying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms'
+import { ClipboardModule } from '@angular/cdk/clipboard'
+import { sleep } from '@anthropic-ai/sdk/core.mjs';
 
 @Component({
   selector: 'app-root',
-  imports: [ReactiveFormsModule],
+  standalone: true,
+  imports: [ReactiveFormsModule, ClipboardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'linky';
-  output: string | null | undefined = "Output";
+  output: string = "Output";
   inputForm = new FormGroup({
     url: new FormControl('', Validators.required),
   })
   handleSubmit() {
-    this.output = this.inputForm.value.url;
+    this.output = this.inputForm.value.url ? this.inputForm.value.url : '';
     this.inputForm.reset()
+  }
+  copy = false;
+  handleClick() {
+    this.copy = true
+    setTimeout(()=> {
+      this.copy = false;
+    }, 2000);
   }
 }
