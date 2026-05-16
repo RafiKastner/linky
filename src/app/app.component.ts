60edefa995227af5e701eef530c17ba195308016
RafiKastner <rafikastner@gmail.com>
2026-05-16 01:00:35 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'linky';
+  output: string | null | undefined = "Output";
+  inputForm = new FormGroup({
+    url: new FormControl('', Validators.required),
+  })
+  handleSubmit() {
+    this.output = this.inputForm.value.url;
+    this.inputForm.reset()
+  }
 }
