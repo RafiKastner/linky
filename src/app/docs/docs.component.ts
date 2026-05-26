e9dbbbba06f5dbd66fa8ce34e6d12cda28026b54
RafiKastner <rafikastner@gmail.com>
2026-05-25 23:25:27 -0500

    api docs page

---
+++ src/app/docs/docs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  standalone: true,
+  selector: 'footer-component',
+  templateUrl: './docs.component.html',
+  styleUrl: './docs.component.css',
+})
+export class DocsComponent {
+  
+}
